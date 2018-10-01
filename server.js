ef242e22d6f10d8dd53019ffc8f72149f07b0f10
erickvs <erick.vazquez@yourcal.io>
2018-09-30 20:30:08 -0700

    Added server

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/private-events-app");
+
+// schema
+var nameSchema = new mongoose.Schema({
+ firstName: String,
+ lastNameName: String
+});
+
+var User = mongoose.model("User", nameSchema);
+
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
@@ -14,4 +30,22 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
+
+
+// Stuff from the tutorial to install mongodb
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
